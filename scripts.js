d5229e482130c256cb5731ca74a8325c033f367a
cookethan <ethancook48@gmail.com>
2023-12-20 20:34:08 -0600

    added functionality to resize grid

--- scripts.js
+++ scripts.js
@@ -1,16 +1,20 @@
 const sketchPad = document.querySelector('.sketchPad');
+const slider = document.querySelector('.sizeSlider');
+const sliderValue = document.querySelector('.sliderValue')
 let gridSize = 16
 let drawColor = 'black';
 
 function makeGrid(gridSize){
 
     for(i = 0; i < gridSize; i++){
+        // creates gridRow divs to hold gridBoxes
         let newRow = document.createElement('div');
         newRow.classList.add('gridRow');
         sketchPad.appendChild(newRow);
     }
-    console.log(sketchPad);
+
     for(i = 0; i< gridSize; i ++){
+        // iterates through grid rows to create and add gridBoxes
         for(j=0; j < gridSize; j++){
         let newBox = document.createElement('div');
         newBox.classList.add('gridBox');
@@ -20,8 +24,17 @@ function makeGrid(gridSize){
     }
 }
 
-makeGrid(gridSize);
+slider.oninput = (e) =>{ 
+    gridSize = e.target.value;
+    sliderValue.textContent = `${e.target.value} X ${e.target.value}`
+    sketchPad.replaceChildren();
+    makeGrid(gridSize)
+}
 
 function fill(e){
     e.target.style.backgroundColor = drawColor;
+}
+
+window.onload =  () =>{
+    makeGrid(gridSize);
 }
